9021f634b7947ae60aa331ba5df44ca9b981c083
gokmen.yilmaz <gokmen1977@gmail.com>
2017-11-02 06:34:43 +0300

    oo

---
+++ WpfPublishTest/ViewModel/SiparisViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Pandap.Model.Satis;
+using Pandap.Panda_Model;
+
+namespace WpfPublishTest.ViewModel
+{
+    public class SiparisViewModel : MyBindableBase
+    {
+        private _Siparis _siparis;
+
+        public _Siparis Siparis { get => _siparis; set => _siparis = value; }
+
+        public SiparisViewModel()
+        {
+        }
+    }
+}
